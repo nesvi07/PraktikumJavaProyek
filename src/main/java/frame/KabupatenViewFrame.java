0d1fc4b060bb2486f302a8b02774bca10d5d889f
nesvi07 <nesvi07@gmail.com>
2022-06-30 19:57:39 +0800

    Membuat KabupatenViewFrame

---
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -0,0 +1,34 @@
+package frame;
+
+import javax.swing.*;
+
+public class KabupatenViewFrame extends JFrame{
+    private JPanel mainPanel;
+    private JPanel cariPanel;
+    private JScrollPane viewScrollPane;
+    private JPanel buttonPanel;
+    private JTextField textField1;
+    private JButton cariButton;
+    private JTable viewTable;
+    private JButton tambahButton;
+    private JButton ubahButton;
+    private JButton hapusButton;
+    private JButton batalButton;
+    private JButton cetakButton;
+    private JButton tutupButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+    public KabupatenViewFrame(){
+        init();
+    }
+
+    public void init(){
+        setContentPane(mainPanel);
+        setTitle("Data Kabupaten");
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
