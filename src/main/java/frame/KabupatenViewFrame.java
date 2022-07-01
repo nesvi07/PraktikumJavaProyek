86989d089b2e22103ef63c1e807f99161ddfb38d
nesvi07 <nesvi07@gmail.com>
2022-07-01 19:51:12 +0800

    Read, Search, Delete pada KabupatenViewFrame

--- src/main/java/frame/KabupatenViewFrame.java
+++ src/main/java/frame/KabupatenViewFrame.java
@@ -1,13 +1,19 @@
 package frame;
 
+import helpers.Koneksi;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.*;
 
 public class KabupatenViewFrame extends JFrame{
     private JPanel mainPanel;
     private JPanel cariPanel;
     private JScrollPane viewScrollPane;
     private JPanel buttonPanel;
-    private JTextField textField1;
+    private JTextField cariTextField;
     private JButton cariButton;
     private JTable viewTable;
     private JButton tambahButton;
@@ -20,7 +26,66 @@ public class KabupatenViewFrame extends JFrame{
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
+
     public KabupatenViewFrame(){
+        tutupButton.addActionListener(e -> {
+            dispose();
+        });
+        batalButton.addActionListener(e -> {
+            isiTable();
+        });
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowActivated(WindowEvent e) {
+                isiTable();
+            }
+        });
+        cariButton.addActionListener(e -> {
+            Connection c = Koneksi.getConnection();
+            String keyword = "%" + cariTextField.getText() + "%";
+            String searchSQL = "SELECT * FROM kabupaten WHERE nama like ?";
+            try {
+                PreparedStatement ps = c.prepareStatement(searchSQL);
+                ps.setString(1, keyword);
+                ResultSet rs = ps.executeQuery();
+                DefaultTableModel dtm = (DefaultTableModel) viewTable.getModel();
+                dtm.setRowCount(0);
+                Object[] row = new Object[2];
+                while (rs.next()) {
+                    row[0] = rs.getInt("id");
+                    row[1] = rs.getString("nama");
+                    dtm.addRow(row);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        hapusButton.addActionListener(e -> {
+            int barisTerpilih = viewTable.getSelectedRow();
+            if (barisTerpilih < 0){
+                JOptionPane.showMessageDialog(null, "Pilih data dulu");
+                return;
+            }
+            int pilihan = JOptionPane.showConfirmDialog(null,
+                    "Yakin mau hapus?",
+                    "Konfirmasi Hapus",
+                    JOptionPane.YES_NO_OPTION
+            );
+            if (pilihan == 0){
+                TableModel tm = viewTable.getModel();
+                int id = Integer.parseInt(tm.getValueAt(barisTerpilih, 0).toString());
+                Connection c = Koneksi.getConnection();
+                String deleteSQL = "DELETE FROM kabupaten WHERE id = ?";
+                try {
+                    PreparedStatement ps = c.prepareStatement(deleteSQL);
+                    ps.setInt(1, id);
+                    ps.executeUpdate();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        isiTable();
         init();
     }
 
@@ -31,4 +96,28 @@ public class KabupatenViewFrame extends JFrame{
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
     }
+    public void isiTable(){
+        Connection c = Koneksi.getConnection();
+        String selectSQL = "SELECT * FROM kabupaten";
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+            String header[] = {"Id","Nama Kabupaten"};
+            DefaultTableModel dtm = new DefaultTableModel(header, 0);
+            viewTable.setModel(dtm);
+            viewTable.getColumnModel().getColumn(0).setWidth(32);
+            viewTable.getColumnModel().getColumn(0).setMaxWidth(32);
+            viewTable.getColumnModel().getColumn(0).setMinWidth(32);
+            viewTable.getColumnModel().getColumn(0).setPreferredWidth(32);
+            Object[] row = new Object[2];
+            while (rs.next()) {
+                row[0] = rs.getInt("id");
+                row[1] = rs.getString("nama");
+                dtm.addRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
